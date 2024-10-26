a00a9dc94ee8262cfa3f7e650aeeb044bcf1fae3
Dylan Rodríguez <dylrodriguez@estudiantec.cr>
2024-10-26 17:36:28 -0600

    Antes de recibir los cambios

--- programa/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
+++ programa/ERP/ERP/obj/Debug/net8.0/ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32d36d187f61fc2e1fd1b43010ba739c077c672d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b783e2fc704ad0a94e299bfba6f185e133242b93")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
